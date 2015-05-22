c2e099514d43c11b229c902f57e31dbfe10d93b8
Brian Quinlan <brian@sweetapp.com>
2015-05-22 10:11:18 -0700

    Implement min stack problem.

---
+++ minstack/src/lib.rs
@@ -0,0 +1,165 @@
+//! A stack type with heap-allocated contents.
+//!
+//! MinStacks have `O(1)` push, pop and min (return the minimum value in the
+//! stack).
+
+use std::cmp::PartialEq;
+use std::cmp::PartialOrd;
+
+pub struct MinStack<T> {
+    stack: Vec<T>,
+    min_stack: Vec<T>
+}
+
+impl<T : Copy + PartialEq + PartialOrd> MinStack<T> {
+    /// Constructs a new, empty `MinStack<T>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use minstack::MinStack;
+    ///
+    /// let mut stack: MinStack<i32> = MinStack::new();
+    /// ```
+    #[inline]
+    pub fn new() -> MinStack<T> {
+        MinStack {
+            stack: Vec::new(),
+            min_stack: Vec::new()
+        }
+    }
+
+    /// Appends an element to end of the stack.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the number of elements in the vector overflows a `usize`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use minstack::MinStack;
+    ///
+    /// let mut stack = MinStack::new();
+    /// stack.push(3);
+    /// assert_eq!(Some(3), stack.pop());
+    /// ```
+    #[inline]
+    pub fn push(&mut self, value: T) {
+        self.stack.push(value);
+        match self.min_stack.last() {
+            Some(min) if *min < value => { return; },
+            _ => { }
+        }
+        self.min_stack.push(value);
+    }
+
+    #[inline]
+    fn should_pop_min(&self, value: &Option<T>) -> bool {
+        match *value {
+            Some(v) => match self.min_stack.last() {
+                Some(min) => *min == v,
+                None => false
+            },
+            None => false
+        }
+    }
+
+    /// Removes the last element from the stack and returns it, or `None` if it
+    /// is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use minstack::MinStack;
+    ///
+    /// let mut stack = MinStack::new();
+    /// stack.push(3);
+    /// assert_eq!(Some(3), stack.pop());
+    /// assert_eq!(None, stack.pop());
+    /// ```
+    #[inline]
+    pub fn pop(&mut self) -> Option<T> {
+        let value = self.stack.pop();
+
+        if self.should_pop_min(&value) {
+            self.min_stack.pop();
+        }
+        value
+    }
+
+    /// Returns the smallest element in the stack, or `None` if it is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use minstack::MinStack;
+    ///
+    /// let mut stack = MinStack::new();
+    /// assert_eq!(None, stack.min());
+    /// stack.push(3);
+    /// stack.push(1);
+    /// stack.push(83);
+    /// assert_eq!(Some(&1), stack.min());
+    /// ```
+    #[inline]
+    #[inline]
+    pub fn min(&self) -> Option<&T> {
+        return self.min_stack.last();
+    }
+}
+
+#[test]
+fn empty_stack() {
+    let mut stack : MinStack<i32> = MinStack::new();
+    assert_eq!(None, stack.min());
+    assert_eq!(None, stack.pop());
+}
+
+#[test]
+fn ascending_push() {
+    let mut stack : MinStack<i32> = MinStack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(3), stack.pop());
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(2), stack.pop());
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(1), stack.pop());
+    assert_eq!(None, stack.min());
+    assert_eq!(None, stack.pop());
+}
+
+#[test]
+fn decending_push() {
+    let mut stack : MinStack<i32> = MinStack::new();
+    stack.push(3);
+    stack.push(2);
+    stack.push(1);
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(1), stack.pop());
+    assert_eq!(Some(&2), stack.min());
+    assert_eq!(Some(2), stack.pop());
+    assert_eq!(Some(&3), stack.min());
+    assert_eq!(Some(3), stack.pop());
+    assert_eq!(None, stack.min());
+    assert_eq!(None, stack.pop());
+}
+
+#[test]
+fn duplicate_push() {
+    let mut stack : MinStack<i32> = MinStack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(1);
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(1), stack.pop());
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(2), stack.pop());
+    assert_eq!(Some(&1), stack.min());
+    assert_eq!(Some(1), stack.pop());
+    assert_eq!(None, stack.min());
+    assert_eq!(None, stack.pop());
+}
