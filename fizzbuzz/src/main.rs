145be88b1ccd090fcba330b013e3a09ce8dd2c20
Brian Quinlan <brian@sweetapp.com>
2015-05-27 15:19:23 -0700

    Fizzbuzz implementation

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    for i in 1..101 {
+        match (i % 3, i % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", i) 
+        }
+    }
+}
