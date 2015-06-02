148cfc616028a1af8da5bd23336d37de43193ae6
Brian Quinlan <brian@sweetapp.com>
2015-06-02 10:34:50 -0700

    Enforce uniqueness in the tree.

--- validbtree/src/lib.rs
+++ validbtree/src/lib.rs
@@ -11,13 +11,13 @@ pub enum Link {
 
 fn is_valid_range(node: &Node, min: Option<u32>, max: Option<u32>) -> bool {
     if let Some(m) = min {
-        if node.value < m {
+        if node.value <= m {
             return false;
         }
     }
 
     if let Some(m) = max {
-        if node.value > m {
+        if node.value >= m {
             return false;
         }
     }
@@ -42,12 +42,12 @@ fn is_valid_range(node: &Node, min: Option<u32>, max: Option<u32>) -> bool {
 /// ```
 /// use validbtree::*;
 ///
-/// let n3 = Node { left: Link::Nil, right: Link::Nil, value: 3};
-/// let n7 = Node { left: Link::Nil, right: Link::Nil, value: 7};
+/// let n3 = Node{left: Link::Nil, right: Link::Nil, value: 3};
+/// let n7 = Node{left: Link::Nil, right: Link::Nil, value: 7};
 ///
-/// let n5 = Node { left: Link::Child(Box::new(n7)),
-///                 right: Link::Child(Box::new(n3)),
-///                 value: 5};
+/// let n5 = Node {left: Link::Child(Box::new(n7)),
+///                right: Link::Child(Box::new(n3)),
+///                value: 5};
 /// assert!(!is_valid(&n5));
 /// ```
 pub fn is_valid(node: &Node) -> bool {
@@ -66,20 +66,20 @@ fn valid_tree() {
     //                 5   23
     //                / \   \
     //               3   7   25
-    let n3 = Node { left: Link::Nil, right: Link::Nil, value: 3};
-    let n7 = Node { left: Link::Nil, right: Link::Nil, value: 7};
-    let n25 = Node { left: Link::Nil, right: Link::Nil, value: 25};
+    let n3 = Node{left: Link::Nil, right: Link::Nil, value: 3};
+    let n7 = Node{left: Link::Nil, right: Link::Nil, value: 7};
+    let n25 = Node{left: Link::Nil, right: Link::Nil, value: 25};
 
-    let n5 = Node { left: Link::Child(Box::new(n3)),
-                    right: Link::Child(Box::new(n7)),
-                    value: 5};
-    let n23 = Node { left: Link::Nil, 
-                     right: Link::Child(Box::new(n25)),
-                     value: 23};
+    let n5 = Node{left: Link::Child(Box::new(n3)),
+                  right: Link::Child(Box::new(n7)),
+                  value: 5};
+    let n23 = Node{left: Link::Nil, 
+                   right: Link::Child(Box::new(n25)),
+                   value: 23};
 
-    let n9 = Node { left: Link::Child(Box::new(n5)),
-                    right: Link::Child(Box::new(n23)),
-                    value: 9};
+    let n9 = Node{left: Link::Child(Box::new(n5)),
+                  right: Link::Child(Box::new(n23)),
+                  value: 9};
 
     assert!(is_valid(&n9));
 }
@@ -91,20 +91,44 @@ fn leaf_in_left_subtree_greater_than_root() {
     //                 5   23
     //                / \   \
     //               3  11   25
-    let n3 = Node { left: Link::Nil, right: Link::Nil, value: 3};
-    let n11 = Node { left: Link::Nil, right: Link::Nil, value: 11};
-    let n25 = Node { left: Link::Nil, right: Link::Nil, value: 25};
+    let n3 = Node{left: Link::Nil, right: Link::Nil, value: 3};
+    let n11 = Node{left: Link::Nil, right: Link::Nil, value: 11};
+    let n25 = Node{left: Link::Nil, right: Link::Nil, value: 25};
 
-    let n5 = Node { left: Link::Child(Box::new(n3)),
-                    right: Link::Child(Box::new(n11)),
-                    value: 5};
-    let n23 = Node { left: Link::Nil, 
-                     right: Link::Child(Box::new(n25)),
-                     value: 23};
+    let n5 = Node{left: Link::Child(Box::new(n3)),
+                  right: Link::Child(Box::new(n11)),
+                  value: 5};
+    let n23 = Node{left: Link::Nil, 
+                   right: Link::Child(Box::new(n25)),
+                   value: 23};
 
-    let n9 = Node { left: Link::Child(Box::new(n5)),
-                    right: Link::Child(Box::new(n23)),
-                    value: 9};
+    let n9 = Node{left: Link::Child(Box::new(n5)),
+                  right: Link::Child(Box::new(n23)),
+                  value: 9};
 
     assert!(!is_valid(&n9));
+}
+
+#[test]
+fn duplicate_in_left_sub_tree() {
+    //                   9
+    //                  /
+    //                 9
+    let leaf = Node{left: Link::Nil, right: Link::Nil, value: 9};
+    let root = Node{left: Link::Child(Box::new(leaf)), 
+                    right: Link::Nil,
+                    value: 9};
+        assert!(!is_valid(&root));
+}
+
+#[test]
+fn duplicate_in_right_sub_tree() {
+    //               9 
+    //                \
+    //                 9
+    let leaf = Node{left: Link::Nil, right: Link::Nil, value: 9};
+    let root = Node{left: Link::Nil, 
+                    right: Link::Child(Box::new(leaf)),
+                    value: 9};
+        assert!(!is_valid(&root));
 }
