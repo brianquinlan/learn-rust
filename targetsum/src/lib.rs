1799e19dd4ea38774495154f4ec20eb99e1d9f31
Brian Quinlan <brian@sweetapp.com>
2020-03-16 11:18:00 -0700

    Implement a more obvious  interface

--- targetsum/src/lib.rs
+++ targetsum/src/lib.rs
@@ -10,7 +10,7 @@
 /// assert_eq!(None, target_sum(&vec![1, 2, 3, 5, 7, 11, 13], 11));
 /// ```
 pub fn target_sum(search: &[i32], target: i32) -> Option<(i32, i32)> {
-    if search.len() == 0 {
+    if search.len() < 2 {
         return None;
     }
     // For some reason hash tables are not allowed so sort the vector and
@@ -20,25 +20,16 @@ pub fn target_sum(search: &[i32], target: i32) -> Option<(i32, i32)> {
     let mut lowest_index = 0;
     let mut highest_index = sorted_search.len() - 1;
 
-    while lowest_index <= highest_index {
-        match sorted_search.get(lowest_index) {
-            Some(low) => match sorted_search.get(highest_index) {
-                Some(high) => {
-                    if low + high == target {
-                        return Some((*low, *high));
-                    }
-                    if low + high > target {
-                        if highest_index == 0 {
-                            return None;
-                        }
-                        highest_index -= 1;
-                    } else {
-                        lowest_index += 1;
-                    }
-                },
-                None => { break; }
-            },
-            None => { break; }
+    while lowest_index < highest_index {
+        let low = sorted_search[lowest_index];
+        let high = sorted_search[highest_index];
+
+        if low + high == target {
+            return Some((low, high));
+        } else if low + high > target {
+            highest_index -= 1;
+        } else {
+            lowest_index += 1;
         }
     }
     None
@@ -52,17 +43,28 @@ fn search_vector_empty() {
 #[test]
 fn search_vector_size_one() {
     assert_eq!(None, target_sum(&vec![5], 9));
-    assert_eq!(Some((5, 5)), target_sum(&vec![5], 10));
+    assert_eq!(None, target_sum(&vec![5], 5));
+    assert_eq!(None, target_sum(&vec![5], 10));
 }
 
 #[test]
 fn target_at_edges() {
-    assert_eq!(Some((5, 5)), target_sum(&vec![1, 2, 5, 10], 10));
+    assert_eq!(Some((1, 10)), target_sum(&vec![1, 2, 5, 10], 11));
 }
 
 #[test]
 fn target_same_number() {
     assert_eq!(Some((2, 2)), target_sum(&vec![1, 2, 2, 5, 10], 4));
+}
+
+#[test]
+fn target_same_number_appears_once() {
+    assert_eq!(None, target_sum(&vec![1, 2, 5, 10], 2));
+}
+
+#[test]
+fn target_double_number() {
+    assert_eq!(None, target_sum(&vec![1, 2, 5, 10], 4));
 }
 
 #[test]
